2022b08514eeed0e75b13803b54520ff45fbd745
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-09-15 00:43:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,42 +2,7 @@ resource "random_id" "suffix" {
   byte_length = 3
 }
 
-resource "aws_vpc" "demo" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = { Name = "sentinel-demo-vpc" }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.demo.id
-  tags   = { Name = "sentinel-demo-igw" }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.demo.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true   # public subnet
-  tags = { Name = "sentinel-demo-public" }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.demo.id
-  tags   = { Name = "sentinel-demo-rt" }
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route" "public_internet" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_security_group" "bad_open" {
+resource "aws_security_group" "ec2_security_group" {
   name        = "sentinel-demo-open-sg-${random_id.suffix.hex}"
   description = "Intentionally open SG to trigger Sentinel/CIS"
   vpc_id      = aws_vpc.demo.id
@@ -70,7 +35,7 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_instance" "bad_public" {
+resource "aws_instance" "public-ip" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public.id
@@ -80,11 +45,22 @@ resource "aws_instance" "bad_public" {
   tags = { Name = "sentinel-demo-ec2-public" }
 }
 
-resource "aws_ebs_volume" "bad_unencrypted" {
+resource "aws_ebs_volume" "hashiconf-volume" {
   availability_zone = "${var.region}${var.az_suffix}"
   size              = 8
   encrypted         = false                  
   tags = { Name = "sentinel-demo-ebs-unencrypted" }
 }
 
+resource "aws_s3_bucket" "hashiconf_bucket" {
+  bucket = "sentinel-demo-public-${random_id.suffix.hex}"
+  tags = {
+    Name = "sentinel-demo-public"
+  }
+}
+
+resource "aws_s3_bucket_acl" "public_acl" {
+  bucket = aws_s3_bucket.bad_public.id
+  acl    = "public-read"
+}
 
