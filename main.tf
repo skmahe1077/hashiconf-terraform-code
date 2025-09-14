52723c9951d7de545bb34b6daa9ed91d63c5a14b
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-09-15 00:51:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,23 @@
-resource "random_id" "suffix" {
-  byte_length = 3
+# Fetch the default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_security_group" "ec2_security_group" {
-  name        = "sentinel-demo-open-sg-${random_id.suffix.hex}"
-  description = "Intentionally open SG to trigger Sentinel/CIS"
-  vpc_id      = aws_vpc.demo.id
-  ingress {
-    description      = "SSH open to world"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = { Name = "sentinel-demo-open-sg" }
+# Fetch the default security group of the default VPC
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
 }
 
+# Get the subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Latest Amazon Linux 2023 AMI
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -35,21 +28,25 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_instance" "public-ip" {
+resource "aws_instance" "hashiconf_ec2_public" {
   ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public.id
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [data.aws_security_group.default.id]
   associate_public_ip_address = true 
-  vpc_security_group_ids      = [aws_security_group.bad_open.id]
 
-  tags = { Name = "sentinel-demo-ec2-public" }
+  tags = {
+    Name = "hashiconf-ec2-public"
+  }
 }
 
-resource "aws_ebs_volume" "hashiconf-volume" {
-  availability_zone = "${var.region}${var.az_suffix}"
+resource "aws_ebs_volume" "hashiconf_volume_unencrypted" {
+  availability_zone = "${var.region}a"
   size              = 8
-  encrypted         = false                  
-  tags = { Name = "sentinel-demo-ebs-unencrypted" }
+  encrypted         = false
+  tags = {
+    Name = "hashiconf-volume-unencrypted"
+  }
 }
 
 resource "aws_s3_bucket" "hashiconf_bucket" {
