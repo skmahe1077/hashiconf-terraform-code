cacc225a35fe52b192c8f4a13546eecf710327cd
Mahendran Selvakumar <31248024+skmahe1077@users.noreply.github.com>
2025-09-15 01:30:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,9 +87,9 @@ resource "aws_s3_bucket" "hashiconf_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "hashiconf_bucket_block" {
   bucket                  = aws_s3_bucket.hashiconf_bucket.id
-  block_public_acls        = false
-  block_public_policy      = false
-  ignore_public_acls       = false
-  restrict_public_buckets  = false
+  block_public_acls        = true
+  block_public_policy      = true
+  ignore_public_acls       = true
+  restrict_public_buckets  = true
 }
 
